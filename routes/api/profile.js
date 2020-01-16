422a1a3acfe30f108fe7b6f43eb27edfdfdd608b
Ramlal S <54655996+ramlal-s@users.noreply.github.com>
2020-01-16 21:00:08 +0530

    user profile inprogress

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('');
 
-//@route    GET api/profile
-//@desc     Test route
-//@access   Public
+//@route    GET api/profile/me
+//@desc     get current users
+//@access   Private
 
-router.get('/', (req,res)=>res.send('profile route'));
+router.get('/', (req,res)=>{
+    try{
+
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
 
 module.exports = router;
