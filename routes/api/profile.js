26c3412c3dfefce2fa16db885e2717c17bb5dacb
Ramlal S <54655996+ramlal-s@users.noreply.github.com>
2020-02-02 16:30:57 +0530

    delete experence in progress

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -209,7 +209,7 @@ router.put('/experience', [auth,
 
     try{
 
-        const profile = await profile.findOne({user: req.user.id});
+        const profile = await Profile.findOne({user: req.user.id});
         profile.experience.unshift(newExp);
 
         await profile.save();
@@ -219,6 +219,26 @@ router.put('/experience', [auth,
         res.status(500).send('server error - experience')
     }
 
-})
+});
+//@route    DELETE api/profile/experience/:exp_id
+//@desc     DELETE experience from profile
+//@access   Private
+router.delete('/experience/:emp_id', auth, async(req,res)=>{
+    try {
+        const profile = await Profile.findOne({user: req.user.id});
+
+        // Get remove index
+        const removeIndex = profile.experience
+        .map(item=>item.id)
+        .indexOf(req.params.exp_id);
+        profile.experience.splice(removeIndex,1);
+        await profile.save();
+        res.json(profile);        
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+
+});
 
 module.exports = router;
