58feecea939573506cee36f71c979c8c993c7234
Ramlal S <54655996+ramlal-s@users.noreply.github.com>
2020-03-04 10:56:56 +0530

    auth in progress

---
+++ client/src/actions/auth.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import {REGISTER_SUCCESS, REGISTER_FAIL} from './types';
+
+//Register User
+export const register =({name, email, password}) => async dispatch =>{
+    const config ={
+        headers:{
+            'Constant-Type':'application/json'
+        }
+    }
+
+    const body = JSON.stringify({name, email, password});
+    try {
+        const res = await axios.post('./api/users',body,config)
+    } catch (err) {
+        
+    }
+}
